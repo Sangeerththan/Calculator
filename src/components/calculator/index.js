32cfcadf71af292891cd1c965c9a099a11c6b7e7
sangeerththan <sangeerththan.15@cse.mrt.ac.lk>
2021-09-16 02:01:34 +0530

    [FEATURE][DONE - Buttons and control UI Finished

--- src/components/calculator/index.js
+++ src/components/calculator/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container,Screen, Current, Previous } from "./styled";
+import { Container,Screen, Current, Previous, Button } from "./styled";
 
 export default function Calculator() {
     return (
@@ -8,6 +8,24 @@ export default function Calculator() {
             <Previous>10</Previous>
             <Current>10</Current>
             </Screen>
+            <Button gridSpan={2} control>AC</Button>
+            <Button control>DEL</Button>
+            <Button operation>÷</Button>
+            <Button>7</Button>
+            <Button>8</Button>
+            <Button>9</Button>
+            <Button operation>x</Button>
+            <Button>4</Button>
+            <Button>5</Button>
+            <Button>6</Button>
+            <Button operation>+</Button>
+            <Button>1</Button>
+            <Button>2</Button>
+            <Button>3</Button>
+            <Button operation>-</Button>
+            <Button period>.</Button>
+            <Button>0</Button>
+            <Button gridSpan={2} equals>=</Button>
         </Container>
     )        
 }
--- src/components/calculator/styled/index.js
+++ src/components/calculator/styled/index.js
@@ -33,3 +33,21 @@ export const Current = styled.div`
     color: white;
     font-size; 2.5rem;
 `
+export const Button = styled.button`
+    cursor: pointer;
+    font-size: 2rem;
+    border: 1px outset white;
+    outline: none;
+    background-color: rgba(255, 255, 255, 0.75);
+    &:hover {
+        background-color: rgba(255, 255, 255, 0.9);
+    }
+
+    ${({gridSpan}) => gridSpan && `grid-column: span ${gridSpan};`}
+    ${({operation}) => operation && `background-color: gray;`}
+    ${({control}) => control && `background-color: skyblue;`}
+    ${({equals}) => equals && `border-bottom-right-radius: 10px; background-color: gray;`}
+    ${({period}) => period && `border-bottom-left-radius: 10px; background-color: skyblue;`}
+
+
+`
